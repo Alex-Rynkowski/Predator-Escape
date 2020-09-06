785e3c8415a3ef00615800972f833d158100f5c3
Symphysis <alex.rynkowski90@gmail.com>
2020-09-06 20:04:59 +0200

    Added sprint + script
    Added sprint btn and a sprint bar.
    To change the deplete amount go to the "Sprint btn" and on the button you'll se a deplete amount

---
+++ Predator Escape/Predator Escape/Assets/Programming/Display/Stamina.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace PE.Display
+{
+    public class Stamina : MonoBehaviour, IPointerClickHandler
+    {
+        [SerializeField] GameObject staminaBar;
+        [SerializeField] float depleteAmount;
+
+        Text sprintTxt;
+
+        private void Start()
+        {
+            SprintText();
+        }
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Sprint();
+            SprintText();
+        }
+
+        private float Sprint()
+        {
+            float currentAmount = staminaBar.GetComponentInChildren<Image>().fillAmount * 100;
+            float newAmount = currentAmount - depleteAmount;
+
+            print(newAmount);
+            if (newAmount < 0)
+            {
+                return currentAmount;
+            }
+            else
+            {
+                return staminaBar.GetComponentInChildren<Image>().fillAmount -= (depleteAmount / 100);
+            }
+            
+        }
+
+        private void SprintText()
+        {
+            sprintTxt = staminaBar.GetComponentInChildren<Text>();
+            sprintTxt.text = string.Format("Stamina: {0}/100", GetFillAmount());
+        }
+
+        private float GetFillAmount()
+        {
+            return Convert.ToInt32(staminaBar.GetComponentInChildren<Image>().fillAmount * 100);
+        }
+    }
+
+}
