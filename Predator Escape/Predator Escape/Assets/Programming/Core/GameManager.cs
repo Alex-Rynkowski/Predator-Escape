4dad2681fe8abbf7258e7da1a9f83967cf599795
Symphysis <alex.rynkowski90@gmail.com>
2020-09-07 22:31:18 +0200

    Minor changes, nothing important

--- Predator Escape/Predator Escape/Assets/Programming/Core/GameManager.cs
+++ Predator Escape/Predator Escape/Assets/Programming/Core/GameManager.cs
@@ -11,18 +11,31 @@ namespace PE.Core
     public class GameManager : MonoBehaviour
     {
         [SerializeField] Slider musicSlider;
+        [SerializeField] GameObject settingsPanel;
 
         public float musicVolumeSet;
         public Action a_musicSettings;
+
+        GraphicRaycaster m_Raycaster;
+        PointerEventData m_PointerEventData;
+        EventSystem m_EventSystem;
 
         private void Start()
         {
             a_musicSettings += MusicVolumeSettings;
         }
-
+        private void Update()
+        {
+            Settings();
+        }
         private void MusicVolumeSettings()
         {
             musicVolumeSet = FindObjectOfType<SliderSettings>().GetComponent<Slider>().value;            
+        }
+
+        private void Settings()
+        {
+
         }
 
     }
