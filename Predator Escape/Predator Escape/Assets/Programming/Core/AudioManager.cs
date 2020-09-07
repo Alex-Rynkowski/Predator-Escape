8cddc2f74828ac28d6a7d9ba96e7358ee21b8c57
Symphysis <alex.rynkowski90@gmail.com>
2020-09-07 21:43:25 +0200

    Added a bunch of scripts
    Audio manager
    Camera contoller
    Slider settings

---
+++ Predator Escape/Predator Escape/Assets/Programming/Core/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace PE.Core
+{
+    public class AudioManager : MonoBehaviour
+    {
+        [SerializeField] AudioSource[] music;
+
+        public void PlayMusic(int musicToPlay)
+        {
+            for (int i = 0; i < music.Length; i++)
+            {
+                if(musicToPlay == i)
+                {
+                    music[i].GetComponent<AudioSource>().Play();
+                }
+            }
+        }
+    }
+
+}
--- Predator Escape/Predator Escape/Assets/Programming/Core/GameManager.cs
+++ Predator Escape/Predator Escape/Assets/Programming/Core/GameManager.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
+﻿using PR.Display;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+namespace PE.Core
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GameManager : MonoBehaviour
     {
-        
+        [SerializeField] Slider musicSlider;
+
+        public float musicVolumeSet;
+        public Action musicSettings;
+
+        private void Start()
+        {
+            musicSettings += MusicVolumeSettings;
+        }
+
+        private void MusicVolumeSettings()
+        {
+            musicVolumeSet = FindObjectOfType<SliderSettings>().GetComponent<Slider>().value;            
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
