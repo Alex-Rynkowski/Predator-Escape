4a14c76a431a9c3cef1ebac527dc307ba696748f
Symphysis <alex.rynkowski90@gmail.com>
2020-09-08 11:31:21 +0200

    Revert "Merge branch 'master' of https://github.com/Symphysis/Predator-Escape"
    This reverts commit 72e474adc308f9445013f1d642dd9e4caa291e58, reversing
    changes made to 2c77cef5e28929ad5473ff9af903e030ec7b92d2.

---
+++ Predator Escape/Predator Escape/Assets/Programming/Display/SliderSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SliderSettings : MonoBehaviour, IPointerClickHandler, IDragHandler
+{
+    public void OnDrag(PointerEventData eventData)
+    {
+        SliderValueChange();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SliderValueChange();
+    }
+
+    public void SliderValueChange()
+    {
+        FindObjectOfType<GameManager>().a_musicSettings();
+    }
+
+}
