8b584be3a796ddc119e2a4c2a1479518ed239297
Symphysis <alex.rynkowski90@gmail.com>
2020-09-10 20:39:03 +0200

    nothing important
    just a namespace

--- Predator Escape/Predator Escape/Assets/Programming/Movement/BossMovement.cs
+++ Predator Escape/Predator Escape/Assets/Programming/Movement/BossMovement.cs
@@ -2,41 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossMovement : MonoBehaviour
+namespace PE.Movement
 {
-    [SerializeField] float moveSpeed = 10;
+    public class BossMovement : MonoBehaviour
+    {
+        [SerializeField] float moveSpeed = 10;
 
-    Rigidbody2D rb;    
-    float obstHeight;
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-    private void Update()
-    {
-        LookForObstacles();
-    }
-    private void MoveSpeed()
-    {
-        rb.velocity = new Vector2(moveSpeed, 0);
-    }
-
-    private void LookForObstacles()
-    {
-        float rayDist = 5f;
-        Physics2D.queriesStartInColliders = false;
-        Vector3 right = transform.TransformDirection(Vector3.right) * rayDist;
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, right);
-                
-        Debug.DrawRay(transform.position, right, Color.red);
-        if(hit.collider != null)
+        Rigidbody2D rb;
+        float obstHeight;
+        private void Start()
         {
-            if(hit.distance < rayDist)
+            rb = GetComponent<Rigidbody2D>();
+        }
+        private void Update()
+        {
+            LookForObstacles();
+        }
+        private void MoveSpeed()
+        {
+            rb.velocity = new Vector2(moveSpeed, 0);
+        }
+
+        private void LookForObstacles()
+        {
+            float rayDist = 5f;
+            Physics2D.queriesStartInColliders = false;
+            Vector3 right = transform.TransformDirection(Vector3.right) * rayDist;
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, right);
+
+            Debug.DrawRay(transform.position, right, Color.red);
+            if (hit.collider != null)
             {
-                obstHeight = hit.transform.GetComponent<Collider2D>().transform.position.y;
-                print(obstHeight);
-                rb.velocity = new Vector2(0, obstHeight);
+                if (hit.distance < rayDist)
+                {
+                    obstHeight = hit.transform.GetComponent<Collider2D>().transform.position.y;
+                    print(obstHeight);
+                    rb.velocity = new Vector2(0, obstHeight);
+                }
             }
-        }               
+        }
     }
 }
